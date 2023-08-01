8e0223718c31d717952988b27b680ce5b4db49ff
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-01 17:08:59 -0300

    style:  InputTime

--- lib/components/input_time.dart
+++ lib/components/input_time.dart
@@ -2,20 +2,55 @@ import 'package:flutter/material.dart';
 
 class InputTime extends StatelessWidget {
   final String title;
-  final int value;
+  final int timer;
 
   const InputTime({
     super.key,
     required this.title,
-    required this.value,
+    required this.timer,
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(this.title),
-        Text('${this.value}'),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                backgroundColor: Colors.red,
+              ),
+              child: const Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+            ),
+            Text('$timer min '),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+                backgroundColor: Colors.red,
+              ),
+              child: const Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+            )
+          ],
+        )
       ],
     );
   }
