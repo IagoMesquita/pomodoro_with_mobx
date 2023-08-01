0c6fc10c060908639b3c47605b0c8824dc36e2ce
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-01 19:55:06 -0300

    feat: implement ChronometerButton

--- lib/components/chronometer.dart
+++ lib/components/chronometer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro_with_mobx/components/chronometer_button.dart';
 
 class Chronometer extends StatelessWidget {
   const Chronometer({super.key});
@@ -8,13 +9,40 @@ class Chronometer extends StatelessWidget {
     return Container(
       color: Colors.red,
       child: Column(
-        children: const [
-          Text(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
             'Hora de Trabalhar',
             style: TextStyle(
               fontSize: 40,
               color: Colors.white,
             ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            '25:00',
+            style: TextStyle(
+              fontSize: 120,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: ChronometerButton(text: 'Iniciar', icon: Icons.play_arrow),
+              ),
+              // Padding(
+              //   padding: EdgeInsets.only(right: 10),
+              //   child: ChronometerButton(text: 'Parar', icon: Icons.stop),
+              // ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: ChronometerButton(text: 'Reiniciar ', icon: Icons.refresh),
+              ),
+            ],
           ),
         ],
       ),
