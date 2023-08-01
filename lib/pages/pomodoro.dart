c62014c43d0a58f8f40141a02978bf764c572f95
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-01 17:09:14 -0300

    style: Pomodoro

--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:pomodoro_with_mobx/components/input_time.dart';
 
@@ -9,10 +7,18 @@ class Pomodoro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-      InputTime(title: 'Entrada', value: 25),
-      InputTime(title: 'Parada', value: 10),
-      ],),
+      body: Column(
+        children: [
+          const Text('Pomodoro'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              InputTime(title: 'Trabalho', timer: 25),
+              InputTime(title: 'Descanso', timer: 10),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
