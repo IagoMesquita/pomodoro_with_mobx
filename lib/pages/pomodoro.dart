29adcdd68accaf859f10c6d08c8a5419a67d5b3b
Iago Mesquita <iagomesquitaalves@gmail.com>
2023-08-01 22:11:30 -0300

    feat: provider of Pomodoro Store

--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro_with_mobx/components/chronometer.dart';
 import 'package:pomodoro_with_mobx/components/input_time.dart';
+import 'package:pomodoro_with_mobx/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -16,12 +21,24 @@ class Pomodoro extends StatelessWidget {
             padding: const EdgeInsets.symmetric(
               vertical: 40,
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: const [
-                InputTime(title: 'Trabalho', timer: 25),
-                InputTime(title: 'Descanso', timer: 10),
-              ],
+            child: Observer(
+              builder: (_) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  InputTime(
+                    title: 'Trabalho',
+                    timer: store.workingTime,
+                    inc: store.incrementWorkingTime,
+                    dec: store.decrementWorkingTime,
+                  ),
+                  InputTime(
+                    title: 'Descanso',
+                    timer: store.restTime,
+                    inc: store.incrementRestTime,
+                    dec: store.decrementRestTime,
+                  ),
+                ],
+              ),
             ),
           ),
         ],
